ef8e3fbbf63ed2a47cb1610ca48f927287539f89
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-11-18 13:18:55 -0300

    atualizando funcoes

--- python/comparsion.py
+++ python/comparsion.py
@@ -1,23 +1,30 @@
 from process import *
+from selenium import webdriver 
 
-search_this = ["Analista", "Sistemas", "UFSM"]
+def busca():
+    word = words()
+    lista = get_path()    
+    x = -1
+    found = []
+    
+    for i in lista:
+        for j in word:
+            x = i.find(j)
+            if x != -1: found.append(i[i.index(j)])
 
+    print(found)
+    k = NULL
 
-x = -1
-found = []
-
-for i in lista:
-    for j in search_this:
-        x = i.find(j)
-        if x != -1: found.append(i[i.index(j)])
-
-print(found)
-k = NULL
-
-if k != 0:
-    print("Estudante da UFSM")
-else: 
-    print("Nao Estuda na UFSM")
-
-for i in lista:
-    print(i)
+    if k != 0:
+        print(result(0))
+    else: 
+        print(result(1))
+    for i in lista:
+        print(i)
+        
+def verificador(value):
+    if value > 0:
+        return "estudante"
+    else: return "nao estudante"
+    
+busca()
--- python/process.py
+++ python/process.py
@@ -2,24 +2,38 @@ from asyncio.windows_events import NULL
 from PIL import Image
 from pytesseract import pytesseract
 
-#Define path to tessaract.exe
-path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+def catch_images():
+    print("teste")
+ #
+def get_path():
+    #Define path to tessaract.exe
+    path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-profiles = ['images/teste.png', 'images/sampletext1-ocr.png']
+    profiles = ['images/teste.png', 'images/sampletext1-ocr.png']
 
-lista = []
+    lista = []
 
-#Define path to image
-for i in profiles:
-    path_to_image = i
+    #Define path to image
+    for i in profiles:
+        path_to_image = i
 
-    #Point tessaract_cmd to tessaract.exe
-    pytesseract.tesseract_cmd = path_to_tesseract
+        #Point tessaract_cmd to tessaract.exe
+        pytesseract.tesseract_cmd = path_to_tesseract
 
-    #Open image with PIL
-    img = Image.open(path_to_image)
+        #Open image with PIL
+        img = Image.open(path_to_image)
 
-    #Extract text from image
-    text = pytesseract.image_to_string(img)
+        #Extract text from image
+        text = pytesseract.image_to_string(img)
 
-    lista.append(text)
+        lista.append(text)
+        
+        return lista
+
+def words():
+    search_this = ["Analista", "Sistemas", "UFSM"]
+    return search_this
+
+def result(n):
+    result = ["Estudante da UFSM", "Nao Estudante"]
+    return result[n]
